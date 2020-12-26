ae88f6f6030f25db295e841bc19ed2dbb2a6f787
Gosheto012 <65368168+Gosheto012@users.noreply.github.com>
2020-12-26 19:19:54 +0200

    Create Algorithms.h

---
+++ Algorithms.h
@@ -0,0 +1,42 @@
+#ifndef __ALGORITHMS_H
+#define __ALGORITHMS_H
+
+#include"Data.h"
+#include<cstring>
+#include<queue>
+#include<stack>
+
+
+class Algorithms
+{
+    Data loadedGraph;
+    std::string fileName;
+
+    public:
+    
+    Algorithms (std::string b);
+    bool isReachable(std::string start, std::string finish); //1
+
+    bool canReturnToSamePlace (std::string start); //4 
+
+    bool canReachAllOthers (std::string start); // 6
+
+   // void Diijstra (std::string start, std::string finish); 
+    bool stringsBelongInArray (std::string str, std::vector<std::string> strArray);
+
+    bool isNoWayCrossroad (std::string start);//
+    void printOneWayStreets ();//7
+
+
+    void traverse (std::string name, std:: unordered_map< std::string, std:: list<std:: pair<std::string, int> > > map, std::unordered_map<std::string, bool>& m);
+    bool isConnected ();
+    bool samePowerEachEdge (); //5
+    void printEulerianPathIfAvailable (); //5 Hierholzer's algorithm 
+
+    void Diikstra (std::string start, std:: string finish);
+    Data getData ();
+};
+//za dali e svurzan broim dali broqchut stiga raven do broq na dvoikite 
+//moje i nqkva rekursiq->return broinaspisuk 
+
+#endif
