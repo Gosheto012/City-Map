ed96780fa549f08f55f510d5daecd57fbd1bed98
Gosheto012 <65368168+Gosheto012@users.noreply.github.com>
2021-01-05 21:18:58 +0200

    Update Graph.h

--- Graph.h
+++ Graph.h
@@ -1,19 +1,12 @@
-#ifndef __GRAPH_H
-#define __GRAPH_H
-
-#include<iostream>
-#include<list>
-#include<vector>
-#include<string>
 #include<unordered_map>
 #include<utility>
 #include<iterator>
 
 class Graph
 {
-    int numberOfVerices;
-    int numberOfEdges;
-    std:: unordered_map< std::string, std:: list<std:: pair<std::string, int> > > myGraph;
+    private:
+    
+    std::unordered_map<std::string, std:: list<std::pair<std::string, int>>> myGraph;
 
     public:
 
@@ -23,11 +16,15 @@ class Graph
     Graph& operator= (const Graph& other);
 
     void print () const;
-    void addEdge (const std::string& firstVertex, const std::string& secondVertex, int distance);
     bool pathAdjuscent (const std::string& firstVertex, const std::string& secondVertex);
     bool existsVertex (const std::string vertexName);
+    void addEdge (const std::string& firstVertex, const std::string& secondVertex, int distance);
     void addVertex (std::string newName, std::list<std:: pair<std::string, int>>);
-    std:: unordered_map< std::string, std:: list<std:: pair<std::string, int> > > getMyGraph();
+    void removeEdge (const std::string& firstVertex, const std::string& secondVertex);
+    void removeVertex (const std::string& vertexName);
+    int getDistanceConnectedNodes (const std::string& firstVertex, const std::string& secondVertex);
+    std::unordered_map<std::string, std:: list<std:: pair<std::string, int>>> getMyGraph();
+
 };
 
 #endif
